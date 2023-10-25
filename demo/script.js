884c44cbe6f07248d28eec93543eb3d4ed2178eb
nguyenminhkiet221 <146740876+nguyenminhkiet221@users.noreply.github.com>
2023-10-25 20:37:28 +0700

    Update lesson demo

---
+++ demo/script.js
@@ -0,0 +1,10 @@
+// Nhập vào một số nguyên n và in ra bình phương của nó.
+
+// Nhập và ép kiểu
+let n= parseInt(prompt("Please input a number:")); 
+
+// Xử lý, tính toán
+let result = n **2;
+
+// Output
+alert(result);
---
+++ demo/script2.js
@@ -0,0 +1,6 @@
+let chieudai = parseInt(prompt("Please input a number:")); 
+let chieurong = parseInt(prompt("Please input a number:")); 
+let chuvi = (chieudai + chieurong) *2;
+let dientich = chieudai * chieurong;
+alert(chuvi);
+alert(dientich);
