c047bfbadaa04de5aad3e0e51ce9960f891a51d8
nguyenminhkiet221 <146740876+nguyenminhkiet221@users.noreply.github.com>
2023-10-25 20:33:01 +0700

    Update file leson 10

---
+++ buoi_10_18_10_2023/script.js
@@ -0,0 +1,14 @@
+let titleElem =  document.getElementById("title");
+
+console.log(titleElem);
+
+console.log(titleElem.innerHTML);
+
+titleElem.innerHTML = "Javascript Course Syllabus";
+
+let Lesson1Elem =  document.getElementById("lesson-1");
+Lesson_1.innerText = ". Welcome to Javascript";
+let Lesson2Elem =  document.getElementById("lesson-2");
+Lesson_2.innerText = ". Javascript Fundenmental";
+let Lesson3Elem =  document.getElementById("lesson-3");
+Lesson_3.innerText = ". Javascript Intermediate";
